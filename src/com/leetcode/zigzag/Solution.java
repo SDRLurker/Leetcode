93a4585223e047137870f8426c64145e1745bfa2
Ryan Shin <1982sdr@hanmail.net>
2015-07-18 10:10:00 +0900

    ZigZag Conversion - accepted, refactoring (TRY:006)

--- src/com/leetcode/zigzag/Solution.java
+++ src/com/leetcode/zigzag/Solution.java
@@ -2,24 +2,19 @@ package com.leetcode.zigzag;
 
 public class Solution {
     public String convert(String s, int numRows) {
+        if(numRows == 1)    return s;
         StringBuffer sb = new StringBuffer();
         int pluses[] = new int[] {(numRows-1)*2, 0};
-        if(numRows == 1)    pluses[0] = 1;
         for(int i=0;i<numRows;i++) {
             int cur_idx = i, pi=0;
             while(cur_idx<s.length()) {
                 sb.append(s.charAt(cur_idx));
-                if(pluses[pi] > 0) {
-                    cur_idx += pluses[pi++];
-                    if(pi % 2 == 0) pi=0;
-                    if(pluses[pi]==0)   pi=0;
-                } else {
-                    if(pluses[++pi]>0){
-                        cur_idx += pluses[pi];
-                        pi=0;
-                    } else break;
+                while(pluses[pi] == 0){
+                    pi++;
+                    if(pi%2 == 0)   pi = 0;
                 }
-                if(pi % 2 == 0) pi=0;
+                cur_idx += pluses[pi++];
+                if(pi%2 == 0)   pi = 0;
             }
             pluses[0] -= 2;
             pluses[1] += 2;
@@ -27,3 +22,4 @@ public class Solution {
         return sb.toString();
     }
 }
+
