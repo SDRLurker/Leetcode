461308b5634403df34296adcfd114b274f84814a
Ryan Shin <1982sdr@hanmail.net>
2015-07-18 09:51:12 +0900

    ZigZag Conversion - initial commit (TRY:001)

---
+++ src/com/leetcode/zigzag/Solution.java
@@ -0,0 +1,28 @@
+package com.leetcode.zigzag;
+
+public class Solution {
+    public String convert(String s, int numRows) {
+        StringBuffer sb = new StringBuffer();
+        int pluses[] = new int[] {(numRows-1)*2, 0};
+        for(int i=0;i<numRows;i++) {
+            int cur_idx = i, pi=0;
+            while(cur_idx<s.length()) {
+                sb.append(s.charAt(cur_idx))
+                // Line 8: error: ';' expected
+                if(pluses[pi] > 0) {
+                    cur_idx += pluses[pi++];
+                    if(pluses[pi]==0)   pi=0;
+                } else {
+                    if(pluses[++pi]>0){
+                        cur_idx += plus[pi];
+                        pi=0;
+                    } else break;
+                }
+                if(pi % 2 == 0) pi=0;
+            }
+            pluses[0] -= 2;
+            pluses[1] += 2;
+        }
+        return sb.toString();
+    }
+}
