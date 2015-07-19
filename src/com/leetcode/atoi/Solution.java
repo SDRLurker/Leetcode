619fe9d3c416e4659c173c8ab6151d2b0dc2eec0
Ryan Shin <1982sdr@hanmail.net>
2015-07-19 17:30:23 +0900

    String to Integer(atoi) - wrong answer (TRY:001)

---
+++ src/com/leetcode/atoi/Solution.java
@@ -0,0 +1,30 @@
+package com.leetcode.atoi;
+
+public class Solution {
+    public int myAtoi(String str) {
+        int result = 0;
+        boolean minus = false;
+        boolean firstspace = true;
+        for(int i=0;str!=null && i<str.length();i++){
+            char c = str.charAt(i);
+            if(firstspace){
+                if(c==' ' || c=='\t')   continue;
+                else if(c=='-'){
+                    minus = true;
+                    firstspace = false;
+                } 
+                else if(c<'0' || c>'9') break;
+            }
+            if(c>='0' && c<='9'){
+                firstspace = false;
+                if((long)result*10 > Integer.MAX_VALUE)
+                    result = (minus) ? Integer.MIN_VALUE:Integer.MAX_VALUE;
+                result *=10;
+                result += (c - '0');
+            } else if (!firstspace)     break;
+        }
+        
+        if(minus)   result *= -1;
+        return result;
+    }
+}
---
+++ src/com/leetcode/atoi/Testcase.java
@@ -0,0 +1,10 @@
+package com.leetcode.atoi;
+
+public class Testcase {
+	public static void main(String[] args) {
+		Solution s = new Solution();
+        // TRY : 001
+		System.out.println(s.myAtoi("+1"));
+        // Expected(정답): 1, Output: 0
+	}
+}
