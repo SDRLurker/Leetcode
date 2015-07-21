9341cdcd47d1a50696a612f45074dbbd0173192f
Ryan Shin <1982sdr@hanmail.net>
2015-07-21 10:39:13 +0900

    Palindrome Number - accepted (TRY:005)

--- src/com/leetcode/palindrome_number/Solution.java
+++ src/com/leetcode/palindrome_number/Solution.java
@@ -12,13 +12,15 @@ public class Solution {
     public boolean isPalindrome(int x) {
         int high, low;
         if ( x < 0 ) return false;
+        int digit = digits(x);
         while (x > 0) {
             low = x % 10;
-            high = x / (int) Math.pow(10, digits(x) - 1);
+            high = x / (int) Math.pow(10, digit - 1);
             if (high != low)    return false;
             x -= low;
-            x -= high * (int) Math.pow(10, digits(x) - 1);
+            x -= high * (int) Math.pow(10, digit - 1);
             x /= 10;
+            digit -= 2;
         }
         return true;
     }
