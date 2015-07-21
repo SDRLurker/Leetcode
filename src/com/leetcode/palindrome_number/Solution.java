048fda56a6877b723c26e65ab4fd524e7660fc71
Ryan Shin <1982sdr@hanmail.net>
2015-07-21 10:30:02 +0900

    Palindrome Number - wrong answer (TRY:001)

---
+++ src/com/leetcode/palindrome_number/Solution.java
@@ -0,0 +1,23 @@
+package com.leetcode.palindrome_number;
+
+public class Solution {
+    private int digits(int x) {
+        int result = 0;
+        while (x > 0) {
+            x /= 10;
+            result++;
+        }
+        return result;
+    }
+    public boolean isPalindrome(int x) {
+        int high, low;
+        while (x > 0) {
+            low = x % 10;
+            high = x / (int) Math.pow(10, digits(x) - 1);
+            if (high != low)    return false;
+            x -= low;
+            x -= high * (int) Math.pow(10, digits(x) -1);
+        }
+        return true;
+    }
+}
---
+++ src/com/leetcode/palindrome_number/Testcase.java
@@ -0,0 +1,10 @@
+package com.leetcode.palindrome_number;
+
+public class Testcase {
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		// TRY:001
+		System.out.println(s.isPalindrome(-2147483648));
+		// Expected(정답): false, Output: true 
+	}
+}
